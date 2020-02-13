1cb6e2da068560c9fa64427666d27d3f67403012
Frank Kair <frankkair@gmail.com>
2020-02-13 17:14:00 +0000

    Uses fafx::ExchangeRate.get instead of at
    - get fetches the most recent data, not necessarily Date.today

--- src/server.rb
+++ src/server.rb
@@ -8,14 +8,14 @@ before do
 end
 
 get '/' do
-  Fafx::ExchangeRate.at(Date.today, 'GBP', 'USD').to_s
+  Fafx::ExchangeRate.get('GBP', 'USD').to_s
 end
 
 get '/*/to/*' do
   currencies = params['splat']
   to = currencies[0]
   from = currencies[1]
-  result = Fafx::ExchangeRate.at(Date.today, to, from).to_s
+  result = Fafx::ExchangeRate.get(to, from).to_s
   JSON.generate(:result => result)
 end
 
